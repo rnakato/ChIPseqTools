04afd6824aac25dc41f3e98bd75e621e63733f01
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2020-10-05 18:29:48 +0900

    rm DROMPAplus

--- DROMPAplus/data/ideogram/getIdeo.R
+++
@@ -1,7 +0,0 @@
-library(IdeoViz)
-list = list("mm9", "mm10", "hg19", "hg38","dm3","dm6")
-for (build in list) {
-	file <- paste(build, ".tsv", sep = "")
-	print (file)
-	write.table(getIdeo(build), file, quote=F, sep="\t", col.names=F, row.names = F)
-}
--- DROMPAplus/src/common/util.cpp
+++
@@ -1,8 +0,0 @@
-/* Copyright(c) Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
- * All rights reserved.
- */
-#include "util.hpp"
-std::string basename(const std::string &path)
-{
-    return path.substr(path.find_last_of('/') + 1);
-}
