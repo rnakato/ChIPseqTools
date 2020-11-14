8f4889874aabd313266b65351f1c2296b51685c6
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2020-11-14 14:38:35 +0900

    bug fix in gtf2refFlat

--- src/C++/gtf2refFlat.cpp
+++ src/C++/gtf2refFlat.cpp
@@ -11,9 +11,9 @@ variables_map argv_init(int argc, char* argv[])
 {
   options_description allopts("Options");
   allopts.add_options()
-    ("gtf,g", value<string>(), "Gene file")
-    ("name,n", "Output name instead of id")
-    ("unique,u", "Only output one transcript per one gene (default: all transcripts)")
+    ("gtf,g", value<string>(), "Gene annotation file (gtf format)")
+//    ("name,n", "Output name instead of id")
+    ("unique,u", "Output one reference transcript per one gene (default: all transcripts)")
     ("help,h", "Print this message")
     ;
 
@@ -33,8 +33,8 @@ variables_map argv_init(int argc, char* argv[])
     vector<string> opts = {};
     for (auto x: {"gtf"}) {
       if (!values.count(x)) {
-	cerr << "specify --" << x << " option." << endl;
-	exit(0);
+        cerr << "specify --" << x << " option." << endl;
+        exit(0);
       }
     }
     notify(values);
@@ -44,6 +44,51 @@ variables_map argv_init(int argc, char* argv[])
     exit(0);
   }
   return values;
+}
+
+void printRefFlat(const HashOfGeneDataMap &mp, const int32_t unique)
+{
+  std::cout << "#geneName\tname\tchrom\tstrand\ttxStart\ttxEnd\tcdsStart\tcdsEnd\texonCount\texonStarts\texonEnds\tgene type\ttranscript type";
+  if (unique) std::cout << "\t" << "reference transcript name\treference transcript id";
+  else        std::cout << "\t" << "gene name\tgene id";
+  std::cout << std::endl;
+
+  for (auto pair: mp) {
+    for (auto x: pair.second) {
+      if (unique) std::cout << x.second.gname << "\t" << x.second.gid << "\t";
+      else        std::cout << x.second.tname << "\t" << x.second.tid << "\t";
+
+      std::cout << "chr"<< pair.first << "\t"
+                << x.second.strand << "\t"
+                << (x.second.txStart -1) << "\t"
+                << x.second.txEnd << "\t";
+
+      if (x.second.cdsStart) {
+        std::cout << (x.second.cdsStart -1) << "\t" << x.second.cdsEnd << "\t";
+      } else {
+        std::cout << (x.second.txStart -1) << "\t" << x.second.txEnd  << "\t";
+      }
+
+      std::cout << x.second.exonCount << "\t";
+      if (x.second.strand == "+") {
+        for (int32_t i=0; i<x.second.exonCount; ++i) std::cout << (x.second.exon[i].start -1) << ",";
+        std::cout << "\t";
+        for (int32_t i=0; i<x.second.exonCount; ++i) std::cout << x.second.exon[i].end << ",";
+      } else {
+        for (int32_t i=x.second.exonCount-1; i>=0; --i) std::cout << (x.second.exon[i].start -1) << ",";
+        std::cout << "\t";
+        for (int32_t i=x.second.exonCount-1; i>=0; --i) std::cout << x.second.exon[i].end << ",";
+      }
+
+      std::cout << "\t" << x.second.gtype << "\t" << x.second.ttype;
+
+      if (unique) std::cout << "\t" << x.second.tname << "\t" << x.second.tid;
+      else        std::cout << "\t" << x.second.gname << "\t" << x.second.gid;
+
+      std::cout << std::endl;
+    }
+  }
+  return;
 }
 
 int main(int argc, char* argv[])
@@ -56,8 +101,8 @@ int main(int argc, char* argv[])
   //  printMap(tmp);
   //printMap(gmp);
 
-  if (values.count("unique")) printRefFlat(gmp, values.count("name"));
-  else printRefFlat(tmp, values.count("name"));
+  if (values.count("unique")) printRefFlat(gmp, values.count("unique"));
+  else                        printRefFlat(tmp, values.count("unique"));
 
   return 0;
 }
